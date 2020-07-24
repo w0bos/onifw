75b35ff6913e7334a89dad5d9e39e972d5b85d08
w0bos <w0bos@protonmail.com>
2020-07-24 13:28:37 +0200

    Added flag for uninstaller to use uninstaller.py

--- src/core/uninstaller.py
+++ src/core/uninstaller.py
@@ -6,11 +6,10 @@ from os import system as shell
 class Uninstall:
     
     def __init__(self,installDir):
-        self.installDir=installDir
+        self.installDir = installDir
         self.binDir = "/usr/local/bin/onifw"
         print("[*] - Looking for current install")
         try:
-            os.chdir("$HOME")
             shell("rm -rf {}".format(self.installDir))
             shell("sudo rm {}".format(self.binDir))
             shell("pkkill -f oni.py")
